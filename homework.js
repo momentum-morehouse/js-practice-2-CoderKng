c6eb916e39c2aed2be1b207d728a8f8ff4364292
Tyrone Wade <gcmc_tyronewade@yahoo.com>
2020-09-18 11:49:18 -0700

    committing 1st JS practice 2 assignmt

--- homework.js
+++ homework.js
@@ -7,8 +7,11 @@
 // If the potential member is in the array, remove all instances of it from the array.
 function remove(array, member) {
     let returnArray = [ ]
-    for (){}
-}
+    for (let i = 0; i < array.length; i++) {
+     if (array[i]===member){}    
+    }
+   return returnArray
+
 // 2. Revisit your "remove" function. Make sure that it does not change the original
 // array but instead returns a new array.
 // 3. Create a function called "sum" that takes an array of numbers and
@@ -18,7 +21,7 @@ function remove(array, member) {
 // and returns the average of those numbers.
 
 // 5. Create a function called "minimum" that takes an array of numbers and
-// returns the smallest number in that array.
+// returns the smallest {}number in that array.
 
 // 6. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
